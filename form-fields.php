b8367bdc87b28d5165ed605b7843c33412305684
Laura Bokkers <L.A.Bokkers@gmail.com>
2023-10-18 18:03:57 +0200

    issue #12 form field quotes

--- form-fields.php
+++ form-fields.php
@@ -4,30 +4,30 @@
 
 function showFormStart()
 {
-    echo '<form method="POST" action="index.php">';
+    echo "<form method='POST' action='index.php'>";
 }
 
 
 function showFormField($fieldName, $label, $type, $formData, $options = NULL)
 {
-    echo "<label for=$fieldName>$label</label>";
+    echo "<label for='$fieldName'>$label</label>";
 
     switch ($type) {
 
         case "select":
             $fieldValue = $formData[$fieldName];
-            echo "<select name=$fieldName id=$fieldName>";
+            echo "<select name='$fieldName' id='$fieldName'>";
             foreach ($options as $key => $value) {
-                echo "<option value=$key ";
+                echo "<option value='$key'";
                 if ($key == $fieldValue) echo "selected";
                 echo ">" .  $value . "</option>";
             }
-            echo '</select>';
+            echo "</select>";
             break;
         case "textarea":
-            echo "<textarea name=$fieldName";
+            echo "<textarea name='$fieldName'";
             foreach ($options as $key => $value) {
-                echo " $key=$value ";
+                echo " $key='$value' ";
             }
             echo ">" . $formData[$fieldName] . "</textarea>";
             break;
@@ -35,22 +35,22 @@ function showFormField($fieldName, $label, $type, $formData, $options = NULL)
             $fieldValue = $formData[$fieldName];
             foreach ($options as $key => $value) {
                 $radioId = "$fieldName" . "_" . "$key";
-                echo "<input type=$type name=$fieldName id=$radioId ";
+                echo "<input type='$type' name='$fieldName' id='$radioId' ";
                 if ($key == $fieldValue) echo "checked";
-                echo " value=$key>";
-                echo  "<label for=$radioId >$value</label>";
+                echo "value='$key'>";
+                echo "<label for='$radioId'>$value</label>";
             }
             break;
         default:
-            echo "<input type=$type name=$fieldName id=$fieldName value=$formData[$fieldName]>";
+            echo "<input type='$type' name='$fieldName' id='$fieldName' value='$formData[$fieldName]'>";
     }
 
-    echo '</br><span class="error">' . $formData[$fieldName . 'Err'] . '</span></br></br>';
+    echo "</br><span class='error'>" . $formData[$fieldName . 'Err'] . "</span></br></br>";
 }
 
 function showFormEnd($page, $submitButtonText)
 {
-    echo '<input hidden name="page" value="' . $page . '"></input>
-            <button type="submit">' . $submitButtonText . '</button>
-            </form>';
+    echo "<input hidden name='page' value='$page'></input>
+            <button type='submit'>" . $submitButtonText . "</button>
+            </form>";
 }
